d330b096f3fe085bbe20ed8ee10752dae825e4cb
BeedZiBood <beedzibood@gmail.com>
2024-04-28 11:44:16 +0300

    declared equations

--- system_of_equations.h
+++ system_of_equations.h
@@ -1,8 +1,10 @@
-#ifndef MATH_MODELING_SUSTEM_OF_EQUATIONS_H
-#define MATH_MODELING_SUSTEM_OF_EQUATIONS_H
+#ifndef MATH_MODELING_SYSTEM_OF_EQUATIONS_H
+#define MATH_MODELING_SYSTEM_OF_EQUATIONS_H
 
 namespace mashkin
 {
-    double calc_x1(double x);
+    double calc_x1(double x, double p1, double p2, double p3, double p4, double p5, double p6);
+    double calc_x2(double x, double p1, double p2, double p4);
+    double calc_x3(double x1, double p4);
 }
 #endif
