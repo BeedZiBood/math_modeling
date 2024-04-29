9d18dcfa99799032c29a55de1bf18b41a7cc382e
BeedZiBood <beedzibood@gmail.com>
2024-04-29 10:28:08 +0300

    Declared and defined comparison functions

--- comparison_between_doubles.h
+++ comparison_between_doubles.h
@@ -1,8 +1,47 @@
-//
-// Created by beedz on 29.04.2024.
-//
-
 #ifndef MATH_MODELING_COMPARISON_BETWEEN_DOUBLES_H
 #define MATH_MODELING_COMPARISON_BETWEEN_DOUBLES_H
+#include <limits>
+#include <cmath>
 
-#endif //MATH_MODELING_COMPARISON_BETWEEN_DOUBLES_H
+namespace mashkin
+{
+    template<typename TReal>
+    static bool isApproximatelyEqual(TReal a, TReal b, TReal tolerance = std::numeric_limits<TReal>::epsilon())
+    {
+        TReal diff = std::fabs(a - b);
+        if (diff <= tolerance)
+            return true;
+
+        if (diff < std::fmax(std::fabs(a), std::fabs(b)) * tolerance)
+            return true;
+
+        return false;
+    }
+
+    template< class TReal >
+    static bool isDefinitelyGreaterThan(TReal a, TReal b, TReal tolerance = std::numeric_limits<TReal>::epsilon())
+    {
+        TReal diff = a - b;
+        if (diff > tolerance)
+            return true;
+
+        if (diff > std::fmax(std::fabs(a), std::fabs(b)) * tolerance)
+            return true;
+
+        return false;
+    }
+
+    template< class TReal >
+    static bool isDefinitelyLessThan(TReal a, TReal b, TReal tolerance = std::numeric_limits<TReal>::epsilon())
+    {
+        TReal diff = a - b;
+        if (diff < tolerance)
+            return true;
+
+        if (diff < std::fmax(std::fabs(a), std::fabs(b)) * tolerance)
+            return true;
+
+        return false;
+    }
+}
+#endif
