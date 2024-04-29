fe0a0396168e02f6ad25856933b600ac3a2ff880
BeedZiBood <beedzibood@gmail.com>
2024-04-29 16:13:50 +0300

    improved solving_cubic_eqution function

--- solving_cubic_equation.cpp
+++ solving_cubic_equation.cpp
@@ -1,39 +1,14 @@
 #include "solving_cubic_equation.h"
+#include "comparison_between_doubles.h"
 #include <iostream>
 #include <cmath>
-#include <limits>
 #include <numbers>
 
 namespace mashkin
 {
-    template< class TReal >
-    static bool isDefinitelyGreaterThan(TReal a, TReal b, TReal tolerance = std::numeric_limits<TReal>::epsilon())
+    void solve_cubic_equation(std::vector< std::complex< double > >& x, double p1, double p2, double p3, double p4, double p5, double p6)
     {
-        TReal diff = a - b;
-        if (diff > tolerance)
-            return true;
-
-        if (diff > std::fmax(std::fabs(a), std::fabs(b)) * tolerance)
-            return true;
-
-        return false;
-    }
-
-    template< class TReal >
-    static bool isDefinitelyLessThan(TReal a, TReal b, TReal tolerance = std::numeric_limits<TReal>::epsilon())
-    {
-        TReal diff = a - b;
-        if (diff < tolerance)
-            return true;
-
-        if (diff < std::fmax(std::fabs(a), std::fabs(b)) * tolerance)
-            return true;
-
-        return false;
-    }
-
-    void solve_cubic_equation(double* x, double p1, double p2, double p3, double p4, double p5, double p6)
-    {
+        using namespace std::complex_literals;
         double a = -1 - p4;
         double b = p1 - (p2 * p4) + (p4 * p1) - (p4 * p4 * p2) + p4 + 1 - p5 - (p4 * p3) - (p3 * p4 * p4);
         double c = p1 - (p1 * p2 * p4) - (p4 * p4 * p1 * p2) + (p4 * p1) + (p1 * p5) - (p3 * p4 * p6) - (p3 * p2 * p4 * p4) + (p4 * p3) - (p3 * p4 * p4 * p6) - (p3 * p4 * p4 * p4 * p2) + (p3 * p4 * p4);
@@ -43,7 +18,17 @@ namespace mashkin
         double Q = std::pow(p / 3, 3) + std::pow(q / 2, 2);
         if (isDefinitelyGreaterThan(Q, 0.0))
         {
-            std::cout << "My equation has a complex numbers\n";
+
+            double alf = std::cbrt(- q / 2 + std::sqrt(Q));
+            double bet = std::cbrt(- q / 2 - std::sqrt(Q));
+            double y1 = alf + bet;
+            std::complex< double > y2 = -(alf + bet) / 2;
+            y2.imag((alf - bet) / 2 * std::sqrt(3));
+            std::complex< double > y3 = -(alf + bet) / 2;
+            y3.imag((-alf - bet) / 2 * std::sqrt(3));
+            x[0] = y1;
+            x[1] = y2;
+            x[2] = y3;
         }
         else if (isDefinitelyLessThan(Q, 0.0))
         {
@@ -54,8 +39,10 @@ namespace mashkin
                 double second_var = std::sqrt(-3.0 / p);
                 double thrid_var = std::acos((3.0 * q) / (2.0 * p) * second_var);
                 double fourth_var = std::cos(thrid_var / 3.0 - static_cast< double >(i) * 2 * std::numbers::pi_v<double> / 3);
+                std::complex<double> z4 = 1.0 + 2i;
                 y[i] = 2 * first_var * fourth_var;
                 y[i] = 2 * std::sqrt(-p / 3) * std::cos(1 / 3 * std::acos((3 * q) / (2 * p) * std::sqrt(-3 / p)) - i * 2 * std::numbers::pi_v<double> / 3);
+                //std::complex< double > res_y = y[i] - b / (3 * a);
                 x[i] = y[i] - b / (3 * a);
             }
         }
--- solving_cubic_equation.h
+++ solving_cubic_equation.h
@@ -1,8 +1,10 @@
 #ifndef MATH_MODELING_SOLVING_CUBIC_EQUATION_H
 #define MATH_MODELING_SOLVING_CUBIC_EQUATION_H
+#include <vector>
+#include <complex>
 
 namespace mashkin
 {
-    void solve_cubic_equation(double* x, double p1, double p2, double p3, double p4, double p5, double p6);
+    void solve_cubic_equation(std::vector< std::complex< double > >& x, double p1, double p2, double p3, double p4, double p5, double p6);
 }
 #endif
