adfb55f2f2ae534618f219c0cc000e50d3dc341e
BeedZiBood <beedzibood@gmail.com>
2024-04-29 10:23:43 +0300

    declared solving_cubic_equation function

---
+++ solving_cubic_equation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by beedz on 29.04.2024.
+//
+
+#include "solving_cubic_equation.h"
---
+++ solving_cubic_equation.h
@@ -0,0 +1,8 @@
+#ifndef MATH_MODELING_SOLVING_CUBIC_EQUATION_H
+#define MATH_MODELING_SOLVING_CUBIC_EQUATION_H
+
+namespace mashkin
+{
+    void solve_cubic_equation(double* x, double p1, double p2, double p3, double p4, double p5, double p6);
+}
+#endif
