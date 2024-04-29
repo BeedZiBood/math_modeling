4fae677369b9bdd5eb29e3ec5722af832024717a
BeedZiBood <beedzibood@gmail.com>
2024-04-29 11:12:03 +0300

    deleted unused files

--- zeroin.h
+++
@@ -1,6 +0,0 @@
-#ifndef MATH_MODELING_ZEROIN_H
-#define MATH_MODELING_ZEROIN_H
-#include <functional>
-
-double zeroin(double left, double right, std::function< double(double) > f, double tol, int *flag);
-#endif
