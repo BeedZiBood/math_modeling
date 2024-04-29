f5229283474b8d9044d5891fb24ba5fad6b3a385
BeedZiBood <beedzibood@gmail.com>
2024-04-29 16:14:29 +0300

    improved finding x2 and x3 functions

--- finding_x2_and_x3.cpp
+++ finding_x2_and_x3.cpp
@@ -1,5 +1,21 @@
-//
-// Created by beedz on 29.04.2024.
-//
-
 #include "finding_x2_and_x3.h"
+#include <cstddef>
+
+namespace mashkin
+{
+    void find_x3(std::vector< std::complex< double > >& x1, std::vector< std::complex< double > >& x3, double p4)
+    {
+        for (size_t i = 0; i < 3; i++)
+        {
+            x3[i] = x1[i] / (1 + p4);
+        }
+    }
+
+    void find_x2(std::vector< std::complex< double > >& x1, std::vector< std::complex< double > >& x2, double p1, double p2, double p4)
+    {
+        for (size_t i = 0; i < 3; i++)
+        {
+            x2[i] = (p2 * p4 * x1[i] - x1[i] + std::pow(x1[i], 2)) / (p1 - x1[i]);
+        }
+    }
+}
--- finding_x2_and_x3.h
+++ finding_x2_and_x3.h
@@ -1,8 +1,11 @@
-//
-// Created by beedz on 29.04.2024.
-//
-
 #ifndef MATH_MODELING_FINDING_X2_AND_X3_H
 #define MATH_MODELING_FINDING_X2_AND_X3_H
+#include <vector>
+#include <complex>
 
-#endif //MATH_MODELING_FINDING_X2_AND_X3_H
+namespace mashkin
+{
+    void find_x2(std::vector< std::complex< double > >& x1, std::vector< std::complex< double > >& x2, double p1, double p2, double p4);
+    void find_x3(std::vector< std::complex< double > >& x1, std::vector< std::complex< double > >& x3, double p4);
+}
+#endif
